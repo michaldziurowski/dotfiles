8a7d60117f627b57c6bdbcca6f93cdcdfc34a853
Michal Dziurowski <michal.dziurowski@brainly.com>
2022-10-31 16:22:47 +0100

    add markdown preview - glow

--- .config/nvim/lua/my/plugins.lua
+++ .config/nvim/lua/my/plugins.lua
@@ -121,6 +121,9 @@ return packer.startup(function(use)
         require('git-conflict').setup()
     end }
 
+    -- markdown preview
+    use {"ellisonleao/glow.nvim"}
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
