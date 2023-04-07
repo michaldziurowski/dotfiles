1f145f056a1bace4abeec15f958278c154f704f3
Michal Dziurowski <michal.dziurowski@brainly.com>
2023-04-07 09:01:14 +0200

    terraform and css in treesitter and lazy-lock

--- .config/nvim/lua/my/treesitter.lua
+++ .config/nvim/lua/my/treesitter.lua
@@ -1,6 +1,6 @@
 require("nvim-treesitter.configs").setup {
-    ensure_installed = { "bash", "go", "gomod", "graphql", "html", "hcl", "javascript", "json", "lua", "make", "markdown",
-        "typescript", "yaml", "query" },
+    ensure_installed = { "bash", "go", "gomod", "graphql", "html", "css", "hcl", "javascript", "json", "lua", "make", "markdown",
+        "typescript", "yaml","terraform", "query" },
     highlight = { enable = true, additional_vim_regex_hihlighting = false }
 }
 
