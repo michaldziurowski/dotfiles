0ac3fa49d9baad1adf4512a8d756ba92b5168db4
Michal Dziurowski <michal.dziurowski@brainly.com>
2023-12-06 10:31:03 +0100

    keep yanked text in register on paste

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("v", "p", '"_dP', opts) -- this is kind of important one, when you yank and then paste on visual it allows you to keep what you originaly yanked instead of replacing it with what was in selection
