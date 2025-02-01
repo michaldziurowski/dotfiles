6972a20d19a9d82112738694b5d1f2e2be2b0c64
Michal Dziurowski <mdziurowski@gmail.com>
2025-02-01 17:43:26 +0100

    neotree background color change

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,56 @@
+-- return {
+--   {
+--     "olimorris/onedarkpro.nvim",
+--     priority = 1000, -- Ensure it loads first
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "onedark",
+--     },
+--   },
+-- }
+-- taken from https://github.com/navarasu/onedark.nvim/blob/master/lua/onedark/palette.lua warm palette
+local c = {
+  black = "#191a1c",
+  bg0 = "#2c2d30",
+  bg1 = "#35373b",
+  bg2 = "#3e4045",
+  bg3 = "#404247",
+  bg_d = "#242628",
+  bg_blue = "#79b7eb",
+  bg_yellow = "#e6cfa1",
+  fg = "#b1b4b9",
+  purple = "#c27fd7",
+  green = "#99bc80",
+  orange = "#c99a6e",
+  blue = "#68aee8",
+  yellow = "#dfbe81",
+  cyan = "#5fafb9",
+  red = "#e16d77",
+  grey = "#646568",
+  light_grey = "#8b8d91",
+  dark_cyan = "#316a71",
+  dark_red = "#914141",
+  dark_yellow = "#8c6724",
+  dark_purple = "#854897",
+  diff_add = "#32352f",
+  diff_delete = "#342f2f",
+  diff_change = "#203444",
+  diff_text = "#32526c",
+}
 return {
   {
     "navarasu/onedark.nvim",
     opts = {
       style = "warm",
+      highlights = {
+        -- change colors of neotree drawer
+        NeoTreeWinSeparator = { fg = c.bg3, bg = c.bg0 },
+        NeoTreeNormal = { fg = c.fg, bg = c.bg0 },
+        NeoTreeNormalNC = { fg = c.fg, bg = c.bg0 },
+        NeoTreeEndOfBuffer = { fg = c.bg0, bg = c.bg0 },
+      },
     },
   },
   {
